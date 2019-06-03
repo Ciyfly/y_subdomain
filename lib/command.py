0f9e351cba6de597d9565dac51eb64dd940c69cf
Ciyfly <1766241489@qq.com>
2019-06-03 19:12:50 +0800

    :sparkles: 增加 清除print \r 的输出方法

--- lib/command.py
+++ lib/command.py
@@ -3,15 +3,21 @@
 '''
 @Author: recar
 @Date: 2019-05-30 17:49:08
-@LastEditTime: 2019-05-31 15:33:00
+@LastEditTime: 2019-06-03 19:06:40
 '''
 
 def print_log(message):
-    print ("\r [*] {0}".format(message), end="")
+    print ("\r[*] {0}".format(message), end="")
+
+def print_flush():
+    print ("\r", end="")
 
 def print_info(message):
     print(("[+] {0}".format(message)))
 
+def print_debug(message):
+    print("[-] {0}".format(message))
+
 def print_error(message):
-    print(("[-] {0}".format(message)))
+    print(("[error] {0}".format(message)))
 
